0796328cf1dd49e9add816d46ed62429d583eacc
pallavigupta <pallavigupta378@gmail.com>
2025-04-25 17:11:05 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const app = express();
-
+app.use("/user",(req,res,next)=>{
+    console.log("checking user authentication")
+    const token = "xyz"
+    const isUserAuth = token === "xyz"
+    if(!isUserAuth){
+        res.status(301).send("UNAUTHORIZED USER")
+    }else{
+        next()
+    }
+})
 app.get("/user",(req,res)=>{
+    console.log("1st route handler")
     res.send({firstName:"prachi..", lastName:"Gupta"})
     
 })
 
-
-app.get("/test",(req,res)=>{
-    res.send("testing!");
+app.delete("/user",(req,res)=>{
+    console.log("deleting user...")
+    res.send("user deleted,.,")
+    
 })
 
-app.get("/hello",(req,res)=>{
-    res.send("namaste!");
-})
-app.use("/",(req,res)=>{
-    res.send("hellow!678");
-})
 app.listen(7777,()=>{
     console.log("server is running!");
 })
